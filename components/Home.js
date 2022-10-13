3e859e32ac479eaba513317e0f6f61bbeeccc4a0
Bilal <bilalgumus2001@gmail.com>
2022-10-13 20:10:11 +0300

    fix tracking & line-height

--- components/Home.js
+++ components/Home.js
@@ -6,14 +6,14 @@ function Home({ handleOpenMenu }) {
       <div className="hidden lg:block w-[33vw] bg-home bg-no-repeat bg-cover bg-center" />
       <MenuButton onClick={handleOpenMenu} theme="dark" />
       <div className="grow flex justify-center items-center">
-        <h1 className="font-inter text-[48px] xs:text-[64px] sm:text-[96px] lg:text-[128px] 2xl:text-[144px] font-bold leading-none">
+        <h1 className="font-inter text-[48px] xs:text-[64px] sm:text-[96px] lg:text-[128px] 2xl:text-[144px] font-bold leading-[0.88]">
           <span>Bilal</span>
           <br />
           <span>&emsp;&nbsp;Gümüş</span>
           <br />
           <div className="flex">
             <span>Dev.</span>
-            <span className="text-[18px] md:text-[36px] font-normal align-top leading-normal">
+            <span className="text-[16px] xs:text-[18px] sm:text-[36px] font-normal align-top leading-[1.9] lg:leading-[2.5]">
               &#169;2022
             </span>
           </div>
